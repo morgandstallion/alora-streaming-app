41588f36073f9b460e83aedb0db4df292391db0f
morgandstallion <morgansamuel960@gmail.com>
2026-05-28 17:28:01 +0000

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <ul>
+        <li>fofrifn</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+const Homepage = () => {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
+};
+
+export default Homepage;
